586f096d55ee1277a139976193c5679f907c8773
Noah Ratcliff <me@nratcliff.com>
2015-11-30 21:39:18 -0500

    added config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+/* Get your consumer/user key and 
+secret from https://apps.twitter.com
+and replace the values below */
+$oAuthKeys = array(
+    'consumer_key' => 'YOUR_CONSUMER_KEY',
+    'consumer_secret' => 'YOUR_CONSUMER_SECRET',
+    'user_token' => 'YOUR_USER_TOKEN',
+    'user_secret' => 'YOUR_USER_SECRET'
+);
+
+?>
